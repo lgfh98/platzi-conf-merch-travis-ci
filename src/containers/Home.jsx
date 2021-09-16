a29ceca5151a7a8ef74ffbefe7915fce7b5f7fb9
lgfh98 <lgfigueroah@outlook.com>
2021-09-16 12:04:44 -0500

    lista de productos

--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
+import Products from '../components/Products';
+import initialState from '../initialState';
 
-const Home = () => {
-  return <h1>Home</h1>;
-};
-
+const Home = () => <Products products={initialState.products} />;
 export default Home;
