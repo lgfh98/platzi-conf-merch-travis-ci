40ac65526fd1355dc2684475352561f52fa26681
lgfh98 <lgfigueroah@outlook.com>
2021-09-16 11:53:51 -0500

    Layout

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -8,16 +8,20 @@ import NotFound from '../containers/NotFound';
 import Payment from '../containers/Payment';
 import Success from '../containers/Success';
 
+import Layout from '../components/Layout';
+
 const App = () => (
   <BrowserRouter>
-    <Switch>
-      <Route exact path="/" component={Home} />
-      <Route exact path="/checkout" component={Checkout} />
-      <Route exact path="/checkout/information" component={Information} />
-      <Route exact path="/checkout/payment" component={Payment} />
-      <Route exact path="/checkout/success" component={Success} />
-      <Route component={NotFound} />
-    </Switch>
+    <Layout>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/checkout" component={Checkout} />
+        <Route exact path="/checkout/information" component={Information} />
+        <Route exact path="/checkout/payment" component={Payment} />
+        <Route exact path="/checkout/success" component={Success} />
+        <Route component={NotFound} />
+      </Switch>
+    </Layout>
   </BrowserRouter>
 );
 export default App;
