f3aa6da04b1b1816db51aca1d8b7d31784438a6c
lgfh98 <lgfigueroah@outlook.com>
2021-09-16 15:11:09 -0500

    estilos pagos

--- src/containers/Payment.jsx
+++ src/containers/Payment.jsx
@@ -1,7 +1,13 @@
 import React from 'react';
+import '../styles/components/Payment.css';
 
-const Payment = () => {
-  return <h1>Payment</h1>;
-};
-
+const Payment = () => (
+  <div className="Payment">
+    <div className="Payment-content">
+      <h3>Resument del pedido:</h3>
+      <div className="Payment-button">Boton de pago con Paypal</div>
+    </div>
+    <div />
+  </div>
+);
 export default Payment;
