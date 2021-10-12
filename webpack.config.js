d1a5462cab2c0e13f681d1f9c3073ca293dd0be5
lgfh98 <lgfigueroah@outlook.com>
2021-10-12 09:54:19 -0500

    c

--- webpack.config.js
+++ webpack.config.js
@@ -9,7 +9,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.[contenthash].js',
-    publicPath: '/platzi-conf-merch-travis-c',
+    publicPath: '/platzi-conf-merch-travis-ci',
   },
   resolve: {
     extensions: ['.js', '.jsx'],
